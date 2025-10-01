8fa9342d8147faefa97d9995808fd93db545bcb7
RafaGazziero <rafael.gazziero.ifpr.2023@gmail.com>
2025-10-01 15:46:56 -0300

    Add files via upload

---
+++ item3.js
@@ -0,0 +1,19 @@
+var frutas=[]
+var f
+
+do{
+    f=prompt("Digite a fruta: ")
+    frutas.push(f)
+    if(f == ""){
+    alert("Nenhuma fruta foi indicada.")
+    frutas.pop()
+}
+}while(f!=null)
+
+alert(frutas)
+
+
+for(var i=0; i<frutas.length-1;i++)
+{
+    document.write("<h2>"+frutas[i]+"</h2>")
+}
